b76abae44d57de16d3624f4edd9bfe9c219a121f
何俊哲240108 <1231336658@xiaomi.com>
2024-06-09 23:17:05 +0800

    添加Viewpager，实现滑动滚动视图 同时实现点击导航栏图标进行跳转的点击事件

--- HeJunzheApp/app/src/main/java/com/example/hejunzheapp/MainActivity.java
+++ HeJunzheApp/app/src/main/java/com/example/hejunzheapp/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.hejunzheapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.example.hejunzheapp.customview.NavigationItemView;
+import com.example.hejunzheapp.viewpager.ViewPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager2 viewPager2;
+
+    private NavigationItemView[] navigationItemView;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +31,31 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        initView();
+
+        setViewPager();
+    }
+
+    private void initView() {
+        viewPager2 = findViewById(R.id.view_pager);
+        navigationItemView = new NavigationItemView[3];
+        navigationItemView[0] = findViewById(R.id.goto_page1);
+        navigationItemView[1] = findViewById(R.id.goto_page2);
+        navigationItemView[2] = findViewById(R.id.goto_page3);
+    }
+
+    private void setViewPager() {
+        viewPager2.setAdapter(new ViewPagerAdapter(this));
+
+        for (int i = 0; i < navigationItemView.length; i++) {
+            int finalI = i;
+            navigationItemView[finalI].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    viewPager2.setCurrentItem(finalI, true);
+                }
+            });
+        }
     }
 }
---
+++ HeJunzheApp/app/src/main/java/com/example/hejunzheapp/viewpager/ViewPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.example.hejunzheapp.viewpager;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    public ViewPagerAdapter(@NonNull Fragment fragment) {
+        super(fragment);
+    }
+
+    public ViewPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0:
+                return new Page1Fragment();
+            case 1:
+                return new Page2Fragment();
+            case 2:
+                return new Page3Fragment();
+            default:
+                return new Page1Fragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
