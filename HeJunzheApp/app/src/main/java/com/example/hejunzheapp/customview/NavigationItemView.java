8b4bf04cbd6b28f00a48f3c885e256191ca963f7
何俊哲240108 <1231336658@xiaomi.com>
2024-06-09 23:02:51 +0800

    添加自定义组件与导航栏

--- HeJunzheApp/app/src/main/java/com/example/hejunzheapp/customview/NavigationItemView.java
+++ HeJunzheApp/app/src/main/java/com/example/hejunzheapp/customview/NavigationItemView.java
@@ -1,10 +1,14 @@
 package com.example.hejunzheapp.customview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 
 import com.example.hejunzheapp.R;
 
@@ -12,15 +16,41 @@ public class NavigationItemView extends LinearLayout {
 
     private ImageView imageView;
     private TextView textView;
+
     public NavigationItemView(Context context) {
         super(context);
 
-        LayoutInflater.from(context).inflate(R.layout.view_navigation_item,this);
+        LayoutInflater.from(context).inflate(R.layout.view_navigation_item, this);
+
+        initView();
+    }
+
+    public NavigationItemView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        LayoutInflater.from(context).inflate(R.layout.view_navigation_item, this);
 
         initView();
 
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.NavigationItemView);
 
+        String text = typedArray.getString(R.styleable.NavigationItemView_text);
+        int imageId = typedArray.getResourceId(R.styleable.NavigationItemView_image,R.drawable.baseline_warning_amber_24);
+
+        imageView.setImageResource(imageId);
+        textView.setText(text);
+
+        typedArray.recycle();
     }
+
+    public NavigationItemView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public NavigationItemView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
 
     public void setColor(int color) {
         imageView.setColorFilter(color);
